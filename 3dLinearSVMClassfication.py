066255d0f5d35e19b7a9d9fbddf17f1f64e8dee4
Mihir Sharma <mihirsharmaofficial@gmail.com>
2019-06-07 23:02:48 +0530

    Added decision function and dimensionality reduction
    Decision function for linear SVM added and displayed on 2-D graph (reduced from 3-D)

--- 3dLinearSVMClassfication.py
+++ 3dLinearSVMClassfication.py
@@ -35,4 +35,13 @@ ax.set_xlabel("feature 0")
 ax.set_ylabel("feature 1")
 ax.set_zlabel("feature 1 ** 2")
 
+
+figure2 = plt.figure()
+ZZ = YY ** 2
+dec = linear_svm_3d.decision_function(np.c_[XX.ravel(), YY.ravel(), ZZ.ravel()])
+plt.contourf(XX, YY, dec.reshape(XX.shape), levels = [dec.min(), 0, dec.max()], cmap = mglearn.cm2, alpha = 0.5)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+
 plt.show()
